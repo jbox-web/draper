d58500f8d0a3cc1ecf4bd0872d52a3d6ef2b6084
Nicolas Rodriguez <nico@nicoladmin.fr>
2024-01-28 16:20:36 +0100

    Use class << self instead of def self.

--- lib/draper.rb
+++ lib/draper.rb
@@ -18,48 +18,52 @@ module Draper
     end
   end
 
-  def self.default_controller
-    @default_controller ||= ApplicationController
-  end
+  class << self
 
-  def self.default_controller=(controller)
-    @default_controller = controller
-  end
-
-  def self.decorate(object_or_enumerable, with: nil, namespace: nil)
-    return object_or_enumerable unless decorable?(object_or_enumerable)
-
-    object_or_enumerable = _decorate(object_or_enumerable, with: with, namespace: namespace)
-
-    block_given? ? yield(object_or_enumerable) : object_or_enumerable
-  end
-
-  def self._decorate(object_or_enumerable, with: nil, namespace: nil)
-    klass = _guess_decorator(object_or_enumerable, with: with, namespace: namespace)
-
-    if object_or_enumerable.is_a? Enumerable
-      object_or_enumerable.map { |object| klass.new(object, namespace: namespace.to_s) }
-    else
-      klass.new(object_or_enumerable, namespace: namespace.to_s)
+    def default_controller
+      @default_controller ||= ApplicationController
     end
-  end
 
-  def self._guess_decorator(object_or_enumerable, with: nil, namespace: nil)
-    object_or_enumerable = object_or_enumerable.first if object_or_enumerable.is_a? Enumerable
-    klass     = with || "#{object_or_enumerable.class.name}Decorator"
-    decorator = [namespace, klass].compact.join("::")
-    decorator.safe_constantize || raise(Draper::UninferrableDecoratorError.new(klass, decorator))
-  end
+    def default_controller=(controller)
+      @default_controller = controller
+    end
 
-  def self.decorable?(object_or_enumerable)
-    return false if defined?(ActiveRecord::Relation) \
-      && object_or_enumerable.is_a?(ActiveRecord::Relation) \
-      && object_or_enumerable.blank?
+    def decorate(object_or_enumerable, with: nil, namespace: nil)
+      return object_or_enumerable unless decorable?(object_or_enumerable)
 
-    return false if object_or_enumerable.respond_to?(:empty?) && object_or_enumerable.empty?
-    return false if object_or_enumerable.nil?
-    return false if object_or_enumerable.is_a?(Draper::Decorator)
+      object_or_enumerable = _decorate(object_or_enumerable, with: with, namespace: namespace)
 
-    true
+      block_given? ? yield(object_or_enumerable) : object_or_enumerable
+    end
+
+    def _decorate(object_or_enumerable, with: nil, namespace: nil)
+      klass = _guess_decorator(object_or_enumerable, with: with, namespace: namespace)
+
+      if object_or_enumerable.is_a? Enumerable
+        object_or_enumerable.map { |object| klass.new(object, namespace: namespace.to_s) }
+      else
+        klass.new(object_or_enumerable, namespace: namespace.to_s)
+      end
+    end
+
+    def _guess_decorator(object_or_enumerable, with: nil, namespace: nil)
+      object_or_enumerable = object_or_enumerable.first if object_or_enumerable.is_a? Enumerable
+      klass     = with || "#{object_or_enumerable.class.name}Decorator"
+      decorator = [namespace, klass].compact.join("::")
+      decorator.safe_constantize || raise(Draper::UninferrableDecoratorError.new(klass, decorator))
+    end
+
+    def decorable?(object_or_enumerable)
+      return false if defined?(ActiveRecord::Relation) \
+        && object_or_enumerable.is_a?(ActiveRecord::Relation) \
+        && object_or_enumerable.blank?
+
+      return false if object_or_enumerable.respond_to?(:empty?) && object_or_enumerable.empty?
+      return false if object_or_enumerable.nil?
+      return false if object_or_enumerable.is_a?(Draper::Decorator)
+
+      true
+    end
+
   end
 end
